b8dc43a254a3616f87676c0933343fbc35d57fcc
Adriaan754 <adriaan_brands@icloud.com>
2015-11-21 18:41:35 +0100

    Fixed render hack

--- js/fix-resolution.js
+++ js/fix-resolution.js
@@ -1,16 +1,13 @@
 var deviceWidth = $(window).width();
 var deviceHeight = $(window).height();
-var tooHigh = 767;
-var tooLow = 1119;
-
-
+var tooHigh = 1120;
+var tooLow = 992;
 $(function() {
-  if (deviceWidth < tooLow) {
-    if (deviceHeight > tooHigh) {
-      console.log("Weird deviceWidth, rendering error will happen");
-      document.getElementById("logo-container").style.left = "-2.5em";
-      console.log("Rendering error fixed")
-    }
+  if (deviceWidth <= tooHigh && deviceWidth >= tooLow) {
+    console.log("Weird deviceWidth, rendering error will happen");
+    document.getElementById("logo-container").style.left = "-2.5em";
+    console.log("Rendering error fixed")
+
   } else {
     console.log("Normal deviceWidth, the website will be fine");
   }
