e1a3f9399e9058fc4b7704519655c2f6b1e914ba
Adriaan754 <adriaan_brands@icloud.com>
2015-11-27 21:32:34 +0100

    Finished About Page

--- js/init.js
+++ js/init.js
@@ -1,9 +1,20 @@
-(function($){
-  $(function(){
+(function($) {
+  $(function() {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.materialboxed').materialbox();
+
+    var date = new Date(2000, 05, 28);
+    var date_jannick = new Date(1999, 04, 22);
+
+    function _calculateAge(birthday) { // birthday is a date
+      var ageDifMs = Date.now() - birthday.getTime();
+      var ageDate = new Date(ageDifMs); // miliseconds from epoch
+      return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+    document.getElementById("age-adriaan").innerHTML = "Leeftijd: " + _calculateAge(date);
+    document.getElementById("age-jannick").innerHTML = "Leeftijd: " + _calculateAge(date_jannick);
 
 
   }); // end of document ready
